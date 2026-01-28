4e9235dd0b66621324ff1b0bc4e043175b1b9c2e
edu <edu@AOS-N043.ARCHT1.local>
2026-01-28 15:37:53 +0900

    1/28社員管理システム最終完了

--- spring_crud_exc5_13_Iwai_Mao/src/main/java/jp/co/sss/crud/filter/LoginCheckFilter.java
+++ spring_crud_exc5_13_Iwai_Mao/src/main/java/jp/co/sss/crud/filter/LoginCheckFilter.java
@@ -2,6 +2,8 @@ package jp.co.sss.crud.filter;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Component;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
@@ -10,7 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jp.co.sss.crud.bean.EmployeeBean;
 
-//@Component
+@Component
 public class LoginCheckFilter extends HttpFilter {
 	@Override
 	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
